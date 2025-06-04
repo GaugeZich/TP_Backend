fb9f63c5835848126830684a1b9e039206bd347a
GaugeZich <guilleop.vale@gmail.com>
2025-06-04 19:13:37 -0300

    Route alumno: Get/Post a falta de funcionalidad/controllers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
-import { envs } from "./configuration/envs.js";
+import { envs } from './configuration/envs.js';
+import alumnoRoutes from './routes/alumno.routes.js';
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send("Bienvenido a nuestro servidor!")
+    res.send("Bienvenido a nuestro servidor!");
 });
+
+app.use(alumnoRoutes);
 
 app.set('port', envs.PORT);
 
---
+++ src/routes/alumno.routes.js
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+import { alumnoController } from '../controllers/alumno.controller.js';
+
+const alumnoRoutes = Router();
+
+userRoutes.get('/alumno');
+userRoutes.get('/alumno/:id/materia');
+
+userRoutes.post('/alumno/:id/tarea/:id')
+
+export default userRoutes;

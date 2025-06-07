6d05b645eaad4e58dcc86cc4fe68f1f5dfa74381
Mariano Troncoso <3mariano.7@gmail.com>
2025-06-06 21:49:12 -0300

    Creamos funcion para crear profesores. corección de errores y comentamos lo que falta para hacer

--- src/controllers/profesor.controller.js
+++ src/controllers/profesor.controller.js
@@ -1,2 +1,20 @@
-dasdadskjnklgansdgas }
-sdfgñaslmgas
+import { request, response } from 'express';
+import { getConnection } from '../database/database.js';
+
+
+const create = async (req = request, res = response ) => {
+  const connection = await getConnection();
+  const { nombre, email } = req.body;
+
+  const [result] = await connection.query(
+    'INSERT INTO usuario(nombre, email, rol) VALUES (?, ?, 1)',
+    [nombre, email] 
+  );
+
+  res.status(201).json({ ok: true, result, msg: 'Approved' });
+};
+
+
+
+
+export const profesorController = {create};
--- src/routes/profesor.routes.js
+++ src/routes/profesor.routes.js
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import { profesorController } from '../controllers/profesor.controller.js';
 
-const alumnoRoutes = Router();
+const profesorRoutes = Router();
 
-userRoutes.get('/profesor'); // Muestra todos los profesores
-userRoutes.get('/profesor/:id'); // Muestra un profesor en particular
-userRoutes.get('/profesor/:id/materia'); // Muestra las materias de un profesor
+//userRoutes.get('/profesor'); // Muestra todos los profesores
+//userRoutes.get('/profesor/:id'); // Muestra un profesor en particular
+//userRoutes.get('/profesor/:id/materia'); // Muestra las materias de un profesor
 
-userRoutes.post('/profesor/:id/materia/:id/alumno/:id'); // Profesor matricula a un alumno a una materia
-userRoutes.post('/profesor/:id/materia/:id/tarea'); // Profesor sube una tarea a una materia
+profesorRoutes.post('/profesor', profesorController.create);
+//userRoutes.post('/profesor/:id/materia/:id/alumno/:id'); // Profesor matricula a un alumno a una materia
+//userRoutes.post('/profesor/:id/materia/:id/tarea'); // Profesor sube una tarea a una materia
 
-export default userRoutes;
+export default profesorRoutes;
